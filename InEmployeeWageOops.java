4877152016a957feaa1c46c3636dca69ee8a8a79
“teji1998” <tejasvinirpk@gmail.com>
2020-07-21 22:07:27 +0530

    Added:Interface class for employee wage

---
+++ InEmployeeWageOops.java
@@ -0,0 +1,9 @@
+package empWage;
+interface InEmployeeWageOops 
+{
+  public void addCompanyEmpWage(String name, int empRatePerHr, int numberOfWorkingDays, int maxHrPerMonth);
+
+  public void computeEmpWage();
+
+}
+
